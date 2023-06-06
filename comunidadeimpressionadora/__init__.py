818178786f97ee1ff2a5f0a6e0aadeb58b5aa52e
FreirePython <132616012+FreirePython@users.noreply.github.com>
2023-06-06 11:32:59 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 
 import config
@@ -9,7 +10,11 @@ import config
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'f050ae9c4634d48182a7a336c23967bd'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db' Esse é o caminho para acessar o banco de dados local
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -19,3 +24,4 @@ login_manager.login_message_category = 'alert-info'
 
 
 from comunidadeimpressionadora import routes
+
